f01d942e34c0135aab9323431402f504d96031ca
유민정 <ac07150204@gmail.com>
2026-07-16 16:07:58 +0900

    진료과/의료진 부분

--- hospital-reservation-system.py
+++ hospital-reservation-system.py
@@ -1902,7 +1902,7 @@ def delete_doctor():
     from tabulate import tabulate
 
     # 1. 삭제할 의료진 번호 입력받기
-    doctor_id = input("\n삭제할 의료진번호를 입력하세요 (예: D00122) : ").strip()
+    doctor_id = input("\n삭제할 의료진번호를 입력하세요 (예: D01001) : ").strip()
 
     # CSV 파일 전체 읽기
     try:
